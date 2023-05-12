4165e8c9845914c4b31e337ca39f3e8113bdfc7c
“Erasmo <>
2023-05-12 18:04:21 -0500

    Lab 4 resuelto

---
+++ labs_resueltos/L4/MemoriaExacta-2023-1/build/Debug/MinGW-Windows/MetodoDinamicoExacto.o.d
@@ -0,0 +1,4 @@
+build/Debug/MinGW-Windows/MetodoDinamicoExacto.o: \
+ MetodoDinamicoExacto.cpp MetodoDinamicoExacto.h
+
+MetodoDinamicoExacto.h:
---
+++ labs_resueltos/L4/MemoriaExacta-2023-1/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Jorge Fatama Vera
+ *
+ * Created on 5 de mayo de 2023, 08:13 AM
+ */
+
+#include "MetodoDinamicoExacto.h"
+int main(int argc, char** argv) {
+    char **alumno_Nombre, **curso_Nombre, *alumno_Modalidad;
+    int *alumno_Codigo, *alumno_Porcentaje, *alumno_Escala;
+    int **curso_Alumnos;
+    double *curso_Credito;
+    lecturaAlumnos("alumnos.csv", alumno_Codigo, alumno_Nombre, 
+            alumno_Modalidad, alumno_Porcentaje, alumno_Escala);
+    pruebaLecturaAlumnos("ReporteAlumnos.txt", alumno_Codigo, alumno_Nombre, 
+            alumno_Modalidad, alumno_Porcentaje, alumno_Escala);
+    lecturaCursos("alumnos_cursos.csv", alumno_Codigo, alumno_Escala, 
+            curso_Nombre, curso_Credito, curso_Alumnos);
+    pruebaDeLecturaCursos("ReporteCursos.txt", curso_Nombre, curso_Credito,
+            curso_Alumnos);
+    reporteDeRecaudacionPorModalidad("ReporteRecaudacionModalidad.txt", 
+            alumno_Codigo, alumno_Modalidad, alumno_Porcentaje, alumno_Nombre,
+            curso_Nombre, curso_Credito, curso_Alumnos);
+    return 0;
+}
+
