ccee831558686b4378a9f25a19acc7363ad5b0d8
Era <hector.gomez@pucp.edu.pe>
2024-08-20 17:17:13 -0500

    primer commit 20242

---
+++ 2024-2/Clase01/FuncionesOperadoresSobrecargados/Utils.hpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Utils.hpp
+ * Author: Erasmo G. Montoya
+ *
+ * Created on August 19, 2024, 6:52 PM
+ */
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+
+#endif /* UTILS_HPP */
+
---
+++ 2024-2/Clase01/FuncionesOperadoresSobrecargados/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Erasmo G. Montoya
+ *
+ * Created on August 19, 2024, 6:49 PM
+ */
+
+#include "Utils.hpp"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int *p;
+    *p = 10;  //   p[0]
+    cout<<*p<<endl;
+    return 0;
+}
+
