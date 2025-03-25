6908980402723d5cd15e69b9a72af6654b618875
erasmo gomez <hector.gomez@pucp.edu.pe>
2025-03-25 16:05:27 -0500

    mi primer commit 2025-1

---
+++ 2025-1/Clase01/sobrecarga_funciones/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: hecto
+ *
+ * Created on March 25, 2025, 4:00 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"mi programa progra2"<<endl;
+    return 0;
+}
+
