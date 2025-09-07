26c066f4efa0ea6ebbc950f2e3ef094f1f158107
Erasmo <erasmo.gomez@airnguru.com>
2025-09-07 13:24:23 -0500

    update Data

---
+++ laboratorios_pasados/2024-2/L1/Solucion/Fuentes_Biblioteca_2024_2/src/Sobrecargas.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Erasmo on 4/09/25.
+//
+
+#include "Sobrecargas.hpp"
---
+++ laboratorios_pasados/2024-2/L1/Solucion/Fuentes_Biblioteca_2024_2/src/Sobrecargas.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Erasmo on 4/09/25.
+//
+
+#ifndef L1_SOBRECARGAS_HPP
+#define L1_SOBRECARGAS_HPP
+
+#endif //L1_SOBRECARGAS_HPP
