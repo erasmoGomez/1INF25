8cc64eaa49b4ef4d50d9ce944e9f13d73cbfdd24
erasmoGomez <erasmo@erasmos-MacBook-Air.local>
2026-04-08 18:06:59 -0500

    update clase 06

---
+++ 2026-1/Clase06/Tweets/src/AuxiliaryFunctions.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Erasmo on 2/09/25.
+//
+
+#ifndef TWEETS_AUXILIARYFUNCTIONS_HPP
+#define TWEETS_AUXILIARYFUNCTIONS_HPP
+
+#include "Utils.hpp"
+
+void open_file_read(ifstream &input, const char *nombre_archivo);
+
+void open_file_write(ofstream &output, const char *nombre_archivo);
+
+int read_int(ifstream &input);
+
+double read_double(ifstream &input);
+
+char *read_str(ifstream &input, char delim);
+
+int lookup_int(int to_look, int *source);
+
+char *assign_str(char* source);
+
+#endif //TWEETS_AUXILIARYFUNCTIONS_HPP
---
+++ 2026-1/Clase06/Tweets/src/Utils.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Erasmo on 2/09/25.
+//
+
+#ifndef TWEETS_UTILS_HPP
+#define TWEETS_UTILS_HPP
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#define MAX_TWEETS 50
+using namespace std;
+#endif //TWEETS_UTILS_HPP
