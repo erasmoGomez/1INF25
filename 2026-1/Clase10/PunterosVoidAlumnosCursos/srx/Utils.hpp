aa51286f24f1900f25529323494914759885bb62
erasmoGomez <erasmo@erasmos-MacBook-Air.local>
2026-04-23 08:40:20 -0500

    update clase 09

---
+++ 2026-1/Clase10/PunterosVoidAlumnosCursos/srx/Utils.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Erasmo on 16/09/25.
+//
+
+#ifndef PUNTEROSVOIDALUMNOSCURSOS_UTILS_HPP
+#define PUNTEROSVOIDALUMNOSCURSOS_UTILS_HPP
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#define INCREMENTO 5
+enum registroAlumno {CODIGO, NOMBRE, CURSOS, PROMEDIO};
+enum registroCurso {CODIGOCURSO, NOTA};
+using namespace std;
+#endif //PUNTEROSVOIDALUMNOSCURSOS_UTILS_HPP
