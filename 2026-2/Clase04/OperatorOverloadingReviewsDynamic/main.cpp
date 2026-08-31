73e3374c0806c46bba8f15c15bbb56e36f56dcef
erasmoGomez <erasmo@erasmos-MacBook-Air.local>
2026-08-31 09:28:59 -0500

    update comment

--- 2026-2/Clase04/OperatorOverloadingReviewsDynamic/main.cpp
+++ 2026-2/Clase04/OperatorOverloadingReviewsDynamic/main.cpp
@@ -9,5 +9,18 @@ int main() {
     fill_stats(reviews, n_reviews, stats);
     print_stats(stats, n_reviews);
 
+
+    /*
+     *
+     *struct Reviews reviews; //Nuevo tipo de dato construido por nosotros
+    "Data/reviews.csv"<<reviews; // Las sobrecargas por lo menos deben estar asociadas a un tipo de dato creado por el usuario
+    *
+    *struct Review* reviews; // Esto es un arreglo dinamico o un puntero a Review
+    * // Esto cpp ya no lo considera como un NUEVO TIPO DE DATO
+    * "Data/reviews.csv"<<reviews; // const char* << struct Review* // Punteros NO SON nuevos tipo de datos
+    * //C++ no va a permitir esa sobrecarga
+    * //La solucion es crear una funcion
+    * load_reviews("Data/reviews.csv", reviews, n_reviews);
+     */
     return 0;
 }
