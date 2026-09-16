b07ce2e9c7bee7517d8d55ffa8967bb70e89d783
erasmoGomez <erasmo@erasmos-MacBook-Air.local>
2026-09-16 11:29:31 -0500

    update class 10

---
+++ 2026-2/Class10/QuickSort/Librerias/Utils.hpp
@@ -0,0 +1,11 @@
+//
+// Created by erasmo on 4/23/26.
+//
+
+#ifndef QUICKSORT_UTILS_HPP
+#define QUICKSORT_UTILS_HPP
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+#endif //QUICKSORT_UTILS_HPP
---
+++ 2026-2/Class10/QuickSort/Librerias/funciones.hpp
@@ -0,0 +1,14 @@
+//
+// Created by erasmo on 4/23/26.
+//
+
+#ifndef QUICKSORT_FUNCIONES_HPP
+#define QUICKSORT_FUNCIONES_HPP
+#include "Utils.hpp"
+
+void quick_sort(int *, int, int, double );
+
+void my_swap(int &x, int &y);
+
+void print_array(int *, int, int);
+#endif //QUICKSORT_FUNCIONES_HPP
